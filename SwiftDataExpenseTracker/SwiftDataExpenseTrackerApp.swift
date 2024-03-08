80920d9b2065ecd27710739a5f707819a049a45d
feihuan1 <ericrealtor1997@gmail.com>
2024-03-08 15:08:01 -0800

    Initial Commit

---
+++ SwiftDataExpenseTracker/SwiftDataExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExpenseTrackerApp.swift
+//  SwiftDataExpenseTracker
+//
+//  Created by Feihuan Peng on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
